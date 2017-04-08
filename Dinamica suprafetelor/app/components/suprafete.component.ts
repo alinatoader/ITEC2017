7925d70605e423ce71f6d5b2e64d7905fc624ebd
Vorobet Bogdan <bogdanvorobet@gmail.com>
2017-04-08 14:01:25 +0300

    +editNumeJudetFaraDiacritice +convertire "-" in "?" in incarcaVanzari

--- Dinamica suprafetelor/app/components/suprafete.component.ts
+++ Dinamica suprafetelor/app/components/suprafete.component.ts
@@ -20,20 +20,71 @@ export class SuprafeteComponent implements OnInit{
     incarcaSuprafete(){
         this.suprafeteService.incarcaSuprafete().then(suprafata=> {
                 this.suprafete=suprafata.result.records;
-                
+                this.editSuprafete();
             }
         ).catch(error=>{
             console.log("Eroare la incarcare din API a suprafetelor");
         })
     }
     incarcaVanzari(judet:string){
+        if(judet=="CARAS-SEVERIN")
+            judet="CARAS?SEVERIN";
+        if(judet=="BISTRITA-NASAUD")
+            judet="BISTRITA?NASAUD";
         this.suprafeteService.incarcaVanzari(judet).then(vanzari=>{
             this.vanzari=vanzari.result.records;
         }).catch(error=>{
             console.log("Eroare la incarcare din API a vanzarilor ");
         })
     }
-    
+    editSuprafete(){
+        this.suprafete.forEach(element => {
+            if(element["Judet"] == "ARGE?")
+                element["Judet"]="ARGES";
+            if(element["Judet"] == "BAC?U")
+                element["Judet"]="BACAU";
+            if(element["Judet"] == "BISTRI?A–N?S?UD")
+                element["Judet"]="BISTRITA-NASAUD";
+            if(element["Judet"] == "BOTO?ANI")
+                element["Judet"] = "BOTOSANI";
+            if(element["Judet"]=="BRA?OV")
+                element["Judet"]="BRASOV";
+            if(element["Judet"]=="BR?ILA")
+                element["Judet"]="BRAILA";
+            if(element["Judet"]=="BUCURE?TI")
+                element["Judet"] = "BUCURESTI";  
+            if(element["Judet"]=="BUZ?U")
+                element["Judet"] = "BUZAU";
+            if(element["Judet"]=="CARA?-SEVERIN")
+                element["Judet"]="CARAS-SEVERIN"
+            if(element["Judet"]=="C?L?RA?I")
+                element["Judet"]=="CALARASI"
+            if(element["Judet"]=="CONSTAN?A")
+                element["Judet"]=="CONSTANTA";
+            if(element["Judet"]=="DÂMBOVI?A")
+                element["Judet"]=="DAMBOVITA";
+            if(element["Judet"]=="GALA?I")
+                element["Judet"]=="GALATI";
+            if(element["Judet"]=="IA?I")
+                element["Judet"]=="IASI";
+            if(element["Judet"]=="IALOMI?A")
+                element["Judet"]=="IALOMITA";
+            if(element["Judet"]=="MARAMURE?")
+                element["Judet"]="MARAMURES";
+            if(element["Judet"]=="MEHEDIN?I")
+                element["Judet"]="MEHEDINTI";
+            if(element["Judet"]=="MURE?")
+                element["Judet"]="MURES";
+            if(element["Judet"]=="NEAM?")
+                element["Judet"]="NEAMT";
+            if(element["Judet"]=="S?LAJ")
+                element["Judet"]="SALAJ";
+            if(element["Judet"]=="TIMI?")
+                element["Judet"]="TIMIS";
+            if(element["Judet"]=="VÂLCEA")
+                element["Judet"]="VALCEA";       
+        });
+    }
     ngOnInit() {
         this.incarcaSuprafete();
         this.incarcaVanzari("SUCEAVA");
